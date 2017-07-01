757d47d1db31c3b044c48bd98f5c1ca266b74cbf
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-01 23:21:49 +0000

    Set up a new project max profit

---
+++ max-profit.js
@@ -0,0 +1,9 @@
+/*
+   - create a function maxProfit
+   - should return the maximum profit that could have been made by buying a stock
+     at the given price and then selling the stock later on.
+   - eg: [45, 24, 35, 31, 40, 38, 11]
+   - return 16 (because bought the stock at $24 and sold it at $40, a profit of $16).
+   - if no profit could have been made, return -1.
+*/
+
