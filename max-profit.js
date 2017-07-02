ee5e82daaeb278f337b5ba72ffde508546de22da
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-02 01:08:15 +0000

    Add a parameter called aMaximumProfit with a default value = 0

--- max-profit.js
+++ max-profit.js
@@ -6,16 +6,16 @@
    - return 16 (because bought the stock at $24 and sold it at $40, a profit of $16).
    - if no profit could have been made, return -1.
 */
-  var maximumProfit = 0;
 
-function maxProfit(prices) {
+function maxProfit(prices, aMaximumProfit=0) {
   // base case: pricesArray.length = 2
+  var maximumProfit = aMaximumProfit;
   var noOfElements = prices.length;
   var lastPrice = prices[noOfElements - 1];
   var profit = 0;
 
   if(noOfElements === 1) {
-    return 'end';
+    return maximumProfit;
   } else {
     for(var i = 0; i < noOfElements - 1; i++) {
       console.log(i + ' price: ' + prices[i] + ' last price: ' + lastPrice);
@@ -24,13 +24,11 @@ function maxProfit(prices) {
       if(profit > maximumProfit) {
         maximumProfit = profit;
         console.log(`\t\tmax: ${maximumProfit}`);
-
       }
-
     }
   }
-  return maxProfit(prices.slice(0, noOfElements - 1));
+  return maxProfit(prices.slice(0, noOfElements - 1), maximumProfit);
 }
 
 console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
-// console.log(maxProfit([45, 24, 31]));
+console.log(maxProfit([45, 24, 21, 11, 9, 5]));
