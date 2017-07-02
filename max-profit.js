0bb2642aa7a5db6493a9c1089121e208525b31d1
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-02 00:59:13 +0000

    Add recursive function to find maximum profit

--- max-profit.js
+++ max-profit.js
@@ -6,4 +6,31 @@
    - return 16 (because bought the stock at $24 and sold it at $40, a profit of $16).
    - if no profit could have been made, return -1.
 */
+  var maximumProfit = 0;
 
+function maxProfit(prices) {
+  // base case: pricesArray.length = 2
+  var noOfElements = prices.length;
+  var lastPrice = prices[noOfElements - 1];
+  var profit = 0;
+
+  if(noOfElements === 1) {
+    return 'end';
+  } else {
+    for(var i = 0; i < noOfElements - 1; i++) {
+      console.log(i + ' price: ' + prices[i] + ' last price: ' + lastPrice);
+      profit = lastPrice - prices[i];
+      console.log(`\tprofit: ${profit} x max: ${maximumProfit}`);
+      if(profit > maximumProfit) {
+        maximumProfit = profit;
+        console.log(`\t\tmax: ${maximumProfit}`);
+
+      }
+
+    }
+  }
+  return maxProfit(prices.slice(0, noOfElements - 1));
+}
+
+console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
+// console.log(maxProfit([45, 24, 31]));
